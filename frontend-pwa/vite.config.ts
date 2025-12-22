e05de93529cfc89e58bddb273e7db214afc27ac3
Petr Chalupa <58788289+Petr-Chalupa@users.noreply.github.com>
2025-12-22 17:02:41 +0100

    Base vue pwa setup

---
+++ frontend-pwa/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Catelog",
+                short_name: "Catelog",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#42b983",
+            },
+        }),
+    ],
+});
